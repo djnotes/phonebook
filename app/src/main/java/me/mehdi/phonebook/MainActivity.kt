95ff2ebc130839835130f5c17f1b7dc45ae35155
Mehdi <powergame_coder2@yahoo.com>
2020-08-10 10:35:08 -0400

    Added SQLite database

--- app/src/main/java/me/mehdi/phonebook/MainActivity.kt
+++ app/src/main/java/me/mehdi/phonebook/MainActivity.kt
@@ -1,33 +1,79 @@
 package me.mehdi.phonebook
 
+import android.content.ContentValues
+import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import me.mehdi.phonebook.databinding.ActivityMainBinding
+import me.mehdi.phonebook.databinding.ListItemBinding
+import me.mehdi.phonebook.model.ContactDbHelper
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var db : SQLiteDatabase
+    lateinit var binding: ActivityMainBinding
+    lateinit var cursor: Cursor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button : Button = findViewById(R.id.helloWorld)
+        db = ContactDbHelper(applicationContext).writableDatabase
 
-        val bird : ImageView = findViewById(R.id.imageView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val title : TextView = findViewById(R.id.title)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
-            title.text = "عنوان صفحه"
-            bird.setImageResource(R.drawable.newyork)
+
+        binding.fab.setOnClickListener {
+            val addContactIntent = Intent(this, AddContactActivity::class.java)
+            startActivityForResult(addContactIntent, 100)
+
         }
 
+        updateList()
 
 
     }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            100 -> {
+                val fullName = data?.getStringExtra("fullname")
+                val phone = data?.getStringExtra("phone")
+
+                val values = ContentValues().apply{
+                    put("fullname", fullName)
+                    put("phone", phone)
+                }
+                val rowId = db.insert("person", null, values)
+                if(rowId != -1L){
+                    Toast.makeText(applicationContext, "ذخیره اطلاعات با موفقیت انجام شد", Toast.LENGTH_LONG).show()
+                }
+
+                updateList()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun updateList() {
+        cursor = db.query("person", arrayOf("fullname", "phone"), null, null, null, null, null)
+        cursor.moveToFirst()
+        while(cursor.moveToNext()){
+            val fullName = cursor.getString(cursor.getColumnIndex("fullname"))
+            val phone = cursor.getString(cursor.getColumnIndex("phone"))
+            val parent : LinearLayout = findViewById(R.id.list)
+//            val itemBinding = ListItemBinding.inflate(layoutInflater)
+            val layout = layoutInflater.inflate(R.layout.list_item, parent, false)
+            layout.findViewById<TextView>(R.id.fullName).text = fullName
+            layout.findViewById<TextView>(R.id.phone).text = phone
+            parent.addView(layout)
+
+        }
+        cursor.close()
+    }
 }
---
+++ app/src/main/java/me/mehdi/phonebook/model/ContactDbHelper.kt
@@ -0,0 +1,18 @@
+package me.mehdi.phonebook.model
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ContactDbHelper(context: Context) : SQLiteOpenHelper(context, "contactdb.db", null, 1) {
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL("CREATE TABLE person (fullname TEXT, phone TEXT)")
+        p0?.execSQL("INSERT INTO person (fullname, phone) VALUES ('Hannan Mousavi', '09121231234')")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL("DROP TABLE person")
+        onCreate(p0)
+    }
+}
